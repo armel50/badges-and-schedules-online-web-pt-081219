cb97605a558549dec44ae9fde83b4ca057e261a6
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-12 20:27:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,10 +4,16 @@ end
 
 def batch_badge_creator(array_of_names)
   array_of_badge = []
+  
   array_of_names.each {|name| array_of_badge << badge_maker(name)} 
+  
   array_of_badge
 end
 
 def assign_rooms(speakers_list)
-  
+  list_of_room
+  speakers_list.each do |speaker|
+    list_of_room << "Hello, #{speaker}! You'll be assigned to room #{speakers_list.index(speaker)}"
+    
+  end
 end
